1389b0941d69cd27c6aced92c8fd987748836189
01geek <abc494823@gmail.com>
2024-10-22 21:11:41 +0800

    引入knife4j接口文档

--- src/main/java/com/geek01/yupaoBackend/controller/UserController.java
+++ src/main/java/com/geek01/yupaoBackend/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.geek01.yupaoBackend.controller;
 
+import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Slf4j
 public class UserController {
+
 }
